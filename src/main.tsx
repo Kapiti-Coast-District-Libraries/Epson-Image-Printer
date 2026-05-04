8e0db425a137554144f907a4128f6e5937d2dea4
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-05-04 12:59:40 +1200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,18 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.tsx";
 import UploadPage from "./UploadPage";
+import AdminPage from "./AdminPage"; // 1. Add this import
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/upload" element={<UploadPage />} />
-  </Routes>
-</HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/admin" element={<AdminPage />} /> {/* 2. Add this route */}
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
